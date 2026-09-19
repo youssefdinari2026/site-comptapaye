a84c13a61140d6151faab7aee82b6c5844472dc2
youssefdinari2026 <youssef.dinari@gmail.com>
2026-09-20 00:20:17 +0200

    Add files via upload

--- contact-config.php
+++ contact-config.php
@@ -4,8 +4,10 @@
  * Ce fichier n'est pas accessible depuis le navigateur (voir .htaccess).
  */
 
-// Adresse qui reçoit les demandes. Créez cette boîte dans hPanel > E-mails.
-$CONTACT_TO = 'contact@comptapaie.com';
+// Boîte qui REÇOIT les messages des formulaires : volontairement sur le domaine hébergé chez Hostinger (comptapaye.com).
+// Le courrier de comptapaie.com est chez OVH : un envoi depuis Hostinger vers OVH peut être filtré ou refusé.
+// Pour lire ces messages à une seule adresse, créez dans hPanel > E-mails > Redirections une redirection vers votre boîte habituelle.
+$CONTACT_TO = 'contact@comptapaye.com';
 
 // Adresse d'expédition des notifications : volontairement sur le domaine qui HÉBERGE le site (comptapaye.com),
 // et non comptapaie.com, pour que les e-mails passent les contrôles SPF/DKIM de l'hébergeur (moins de spam).
@@ -13,10 +15,10 @@ $CONTACT_TO = 'contact@comptapaie.com';
 $CONTACT_FROM = 'ComptaPaie <no-reply@comptapaye.com>';
 
 // Adresse qui reçoit les candidatures (stage, alternance, emploi) avec CV en pièce jointe.
-$CAREERS_TO = 'contact@comptapaie.com';
+$CAREERS_TO = 'contact@comptapaye.com';
 
 // Adresse qui reçoit les annonces déposées par les clients (à valider avant publication).
-$ANNONCE_TO = 'contact@comptapaie.com';
+$ANNONCE_TO = 'contact@comptapaye.com';
 
 // Nombre maximum de messages par visiteur et par heure.
 $CONTACT_MAX_PER_HOUR = 5;
